79703c9f1e8ec61775a65bb965928732add95f81
Timothy Boulanger <tboulan@fastmail.com>
2021-10-15 10:52:30 -0700

    MG995 Photoresistor code reformatting

--- MG995_photoresistor_integration_test/MG995_photoresistor_integration_test.ino
+++ MG995_photoresistor_integration_test/MG995_photoresistor_integration_test.ino
@@ -3,10 +3,13 @@
 const int PHOTO_1 = 0;
 const int PHOTO_2 = 1;
 const int SERVO = 3;
+const int SPIN_MOD = 2;
 
 int suggestedSpin = 0;
-unsigned int pulse = 128;
+int pulse = 128;
 int spin = 1;
+const int MIN_PULSE = 50;
+const int MAX_PULSE = 255;
 
 const int threshold_low = 100;
 const int threshold_high = 200;
@@ -19,7 +22,7 @@ void setup() {
   pinMode(A1, INPUT);
   Serial.begin(9600);
 
-  Timer1.initialize(100000);
+  Timer1.initialize(10000);
   Timer1.attachInterrupt(handleServo);
 }
 
@@ -33,18 +36,16 @@ void handleServo() {
 }
 
 void setSuggestedSpin() {
-  int p1, p2;
-  if (!reversed) {
-     p1 = analogRead(PHOTO_1);
-     p2 = analogRead(PHOTO_2);
-  } else {
-     p1 = analogRead(PHOTO_2);
-     p2 = analogRead(PHOTO_1);
+  int spin = 1;
+  if (reversed == true) {
+    spin = -1;
   }
+  int p1 = analogRead(PHOTO_1);
+  int p2 = analogRead(PHOTO_2);
 
   if (p1 > p2) {
     if (p1 - p2 > threshold) {
-      suggestedSpin = 1;
+      suggestedSpin = spin;
       threshold = threshold_low;
     } else if (p1 - p2 < threshold) {
       suggestedSpin = 0;
@@ -52,7 +53,7 @@ void setSuggestedSpin() {
     }
   } else if (p2 > p1) {
     if (p1 - p2 < -threshold) {
-      suggestedSpin = -1;
+      suggestedSpin = -spin;
       threshold = threshold_low;
     } else if (p1 - p2 > -threshold) {
       suggestedSpin = 0;
@@ -62,15 +63,9 @@ void setSuggestedSpin() {
 }
 
 void setPulse() {
-  if (suggestedSpin == -1) {
-    if (pulse > 0) {
-      pulse = pulse + suggestedSpin;
-    }
-  } else if (suggestedSpin == 1) {
-    if (pulse < 255) {
-      pulse = pulse + suggestedSpin;
-    }
-  }
+  pulse = pulse + suggestedSpin * SPIN_MOD;
+  if (pulse <= MIN_PULSE) pulse = MIN_PULSE;
+  if (pulse >= MAX_PULSE) pulse = MAX_PULSE;
   
   Serial.println(pulse);
   analogWrite(SERVO, pulse);
